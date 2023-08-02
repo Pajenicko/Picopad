dca9e2faf6dffba4a27dd1a8461f6b1d65d72019
Vladimír Smitka <vladimir.smitka@lynt.cz>
2023-08-02 18:23:02 +0200

    Picopad photoresistor module example

---
+++ circuitpython/sensors/photoresistor/quick_start/code.py
@@ -0,0 +1,15 @@
+"""
+In this simple example, you will learn how to interface with Picopad photoresistor module.
+It measures light intensity in the surroundings and you can read the value from the analog pin A0.
+"""
+
+import board
+import analogio
+import time
+
+sensor = analogio.AnalogIn(board.A0)
+
+# read and show measured values every second
+while True:
+    print("Light intensity: %d" % sensor.value)
+    time.sleep(1)
